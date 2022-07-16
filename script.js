8a894288d2adfaa9530741bc7d3599a4b3e6fe58
Daiveed <jajadavidjid@gmail.com>
2022-07-16 23:42:33 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+// const year = document.querySelector("#current-year");
+// year.innerHTML = new Date().getFullYear;
+
+// let btn = document.querySelector(".email-button");
+// let email = document.querySelector("input[type=text]");
+// let errMessage = document.querySelector(".form-error");
+// // let errIcon = document.querySelector("#error-icon");
+
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (
+//     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+//       email.value
+//     )
+//   ) {
+//     email.classList.remove("error");
+//     errMessage.classList.remove("form-error");
+//     // errMessage.classList.remove("reveal");
+//     return true;
+//   } else {
+//     email.classList.add("error");
+//     // errIcon.classList.add("error");
+//     errMessage.classList.add("form-error");
+//     errMessage.classList.add("reveal");
+//     setTimeout(() => {
+//       email.classList.remove("error");
+//       // errIcon.classList.remove("error");
+//       errMessage.classList.remove("form-error");
+//       // errMessage.classList.remove("reveal");
+//     }, 3000);
+//     return false;
+//   }
+// });
+
+//  const button = document.querySelector(".form-button");
+//  const input = document.querySelector(".form-input");
+//  const errorText = document.querySelector(".hidden");
+
+//  button.addEventListener("click", (e) => {
+//    e.preventDefault();
+
+//    input.classList.add("error");
+//    errorText.classList.remove("hidden");
+
+//    setTimeout(() => {
+//      input.classList.remove("error");
+//      errorText.classList.add("hidden");
+//    }, 2000);
+//  });
