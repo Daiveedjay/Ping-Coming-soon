b2236aad056d64d27315ded7dd1be12792632235
Daiveed <jajadavidjid@gmail.com>
2022-07-17 21:01:00 +0100

    Second Commit

--- script.js
+++ script.js
@@ -1,50 +1,26 @@
 "use strict";
-// const year = document.querySelector("#current-year");
-// year.innerHTML = new Date().getFullYear;
 
-// let btn = document.querySelector(".email-button");
-// let email = document.querySelector("input[type=text]");
-// let errMessage = document.querySelector(".form-error");
-// // let errIcon = document.querySelector("#error-icon");
+let emailButton = document.querySelector(".email-button");
+let email = document.querySelector("input[type=text]");
+let errP = document.querySelector(".form-error");
 
-// btn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   if (
-//     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-//       email.value
-//     )
-//   ) {
-//     email.classList.remove("error");
-//     errMessage.classList.remove("form-error");
-//     // errMessage.classList.remove("reveal");
-//     return true;
-//   } else {
-//     email.classList.add("error");
-//     // errIcon.classList.add("error");
-//     errMessage.classList.add("form-error");
-//     errMessage.classList.add("reveal");
-//     setTimeout(() => {
-//       email.classList.remove("error");
-//       // errIcon.classList.remove("error");
-//       errMessage.classList.remove("form-error");
-//       // errMessage.classList.remove("reveal");
-//     }, 3000);
-//     return false;
-//   }
-// });
-
-//  const button = document.querySelector(".form-button");
-//  const input = document.querySelector(".form-input");
-//  const errorText = document.querySelector(".hidden");
-
-//  button.addEventListener("click", (e) => {
-//    e.preventDefault();
-
-//    input.classList.add("error");
-//    errorText.classList.remove("hidden");
-
-//    setTimeout(() => {
-//      input.classList.remove("error");
-//      errorText.classList.add("hidden");
-//    }, 2000);
-//  });
+emailButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email.value
+    )
+  ) {
+    email.classList.remove("border-error");
+    errP.classList.remove("form-error");
+    return true;
+  } else {
+    email.classList.add("border-error");
+    errP.classList.add("hidden");
+    setTimeout(() => {
+      email.classList.remove("border-error");
+      errP.classList.remove("hidden");
+    }, 3000);
+    return false;
+  }
+});
